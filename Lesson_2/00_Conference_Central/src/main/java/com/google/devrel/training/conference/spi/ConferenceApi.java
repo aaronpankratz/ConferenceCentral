48e5cd21b71db99e30511d0f1ebfd6a7a0510367
Aaron Pankratz <pankratz.aaron@gmail.com>
2015-03-03 20:14:46 -0600

    add User parameter to saveProfile method, throw exception if user is not authorized, set userId and email from user, extract displayName from email if null

--- Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -46,17 +46,17 @@ public class ConferenceApi {
     // The request that invokes this method should provide data that
     // conforms to the fields defined in ProfileForm
 
-    // TODO 2 Pass the User parameter
-    public Profile saveProfile(ProfileForm profileForm) throws UnauthorizedException {
+    public Profile saveProfile(final User user, ProfileForm profileForm) throws UnauthorizedException {
 
         String userId = null;
         String mainEmail = null;
-        String displayName = "Your name will go here";
+        String displayName = null;
         TeeShirtSize teeShirtSize = TeeShirtSize.NOT_SPECIFIED;
 
-        // TODO 2
         // If the user is not logged in, throw an UnauthorizedException
-        
+        if(user == null) {
+            throw new UnauthorizedException("Authorization required");
+        }
         // Set the teeShirtSize to the value sent by the ProfileForm, if sent
         // otherwise leave it as the default value
         if(profileForm.getTeeShirtSize() != null) {
@@ -69,12 +69,15 @@ public class ConferenceApi {
             displayName = profileForm.getDisplayName();
         }
 
-        // TODO 2
         // Get the userId and mainEmail
+        userId = user.getUserId();
+        mainEmail = user.getEmail();
 
-        // TODO 2
         // If the displayName is null, set it to default value based on the user's email
         // by calling extractDefaultDisplayNameFromEmail(...)
+        if(displayName == null) {
+            displayName = extractDefaultDisplayNameFromEmail(user.getEmail());
+        }
 
         // Create a new Profile entity from the
         // userId, displayName, mainEmail and teeShirtSize
