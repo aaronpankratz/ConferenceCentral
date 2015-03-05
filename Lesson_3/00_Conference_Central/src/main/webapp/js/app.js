8c9289709df09b854aea604afb028abe1ad9e342
Aaron Pankratz <pankratz.aaron@gmail.com>
2015-03-04 19:57:50 -0600

    add client id and tests

--- Lesson_3/00_Conference_Central/src/main/webapp/js/app.js
+++ Lesson_3/00_Conference_Central/src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'replace with your client id',
+        CLIENT_ID: '72112711391-h207r62ejsjt8n4tdkgff73jvkm58sr1.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
