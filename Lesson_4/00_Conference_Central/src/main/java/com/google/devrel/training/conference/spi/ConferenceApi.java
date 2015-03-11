7ad60fe5ef5536966730ba814bf290faafd6e24e
Aaron Pankratz <pankratz.aaron@gmail.com>
2015-03-10 20:20:41 -0500

    add queryConferences method

--- Lesson_4/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ Lesson_4/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -12,9 +12,13 @@ import com.google.devrel.training.conference.Constants;
 import com.google.devrel.training.conference.domain.Conference;
 import com.google.devrel.training.conference.domain.Profile;
 import com.google.devrel.training.conference.form.ConferenceForm;
+import com.google.devrel.training.conference.form.ConferenceQueryForm;
 import com.google.devrel.training.conference.form.ProfileForm;
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.Query;
+
+import java.util.List;
 
 /**
  * Defines conference APIs.
@@ -181,5 +185,24 @@ public class ConferenceApi {
         ofy().save().entities(profile, conference).now();
 
         return conference;
+    }
+
+    /**
+     * Queries against the datastore with the given filters and returns the result.
+     * 
+     * Normally this kind of method is supposed to get invoked by a GET HTTP method,
+     * but we do it with POST, in order to receive conferenceQueryForm Object
+     * 
+     * @param conferenceQueryForm A form object representing the query
+     * @return A List of Conferences that match the query 
+     */
+    @ApiMethod(
+            name = "queryConferences",
+            path = "queryConferences",
+            httpMethod = HttpMethod.POST
+    )
+    public List<Conference> queryConferences(/*ConferenceQueryForm conferenceQueryForm*/) {
+        Query<Conference> query = ofy().load().type(Conference.class);
+        return query.list();
     }
 }
